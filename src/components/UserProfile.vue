739e47ee62979c72258ff1571e55667b137c06d5
camille <camille@cstar.io>
2021-08-26 16:48:23 +0200

    user profile template get user from api

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,52 +1,28 @@
 <template>
   <section class="profilSection">
     <div class="profilPictureContainer">
-      <div class="profilPicture">
+      <!-- <div class="profilPicture">
         <img src="../assets/images/palpatine.jpg" alt="photo de profile" />
-      </div>
-      <div class="profilEditBtn" v-if="edit === false">
-        <button id="editBtn" v-on:click="changeEdit">
-          <a>
-            <router-link to="/user/:id/profile/edit">Editer votre profile</router-link>
-          </a>
-        </button>
-        <button id="deleteBtn">Supprimer votre profile</button>
-      </div>
-      <div class="profilEditBtn" v-else>
-        <button id="validationBtn" v-on:click="changeEdit">
-          <a>
-            <router-link to="/user/:id/profile/">Valider les modifications</router-link>
-          </a>
-        </button>
+      </div> -->
+      <div>
+        <div v-if="!edit">
+          <button @click="changeEdit" class="editBtn profilBtn">Editer</button>
+          <button class="deleteBtn profilBtn">Supprimer</button>
+        </div>
+        <button v-else @click="changeEdit" class="validationBtn profilBtn">Valider</button>
       </div>
     </div>
-    <div class="profilContentContainer">
-      <div class="profilInfos">
-        <h1>{{ userProfil.firstName }} {{ userProfil.name }}</h1>
-        <span>{{ userProfil.subscriptionDate }}</span>
-        <span>{{ userProfil.job }}</span>
+    <div class="profilContentContainer" v-if="userProfile">
+      <div v-if="edit">
+        <form action="">
+          <input type="text" placeholder="nom d'utilisateur" :value="userProfile.username" />
+          <textarea id="story" name="story" rows="5" cols="33" placeholder="Courte bio" :value="userProfile.bio"></textarea>
+        </form>
       </div>
-      <div class="profilContent">
-        <span>Nom d'utilisateur</span>
-        <p>{{ userProfil.userName }}</p>
-        <div v-if="edit === true" class="editInput">
-          <input type="text" placeholder="nom d'utilisateur" />
-        </div>
-        <span>BIO:</span>
-        <p>{{ userProfil.bio }}</p>
-        <div v-if="edit === true" class="editInput">
-          <textarea
-            id="story"
-            name="story"
-            rows="5"
-            cols="33"
-            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim sagittis suscipit."
-          ></textarea>
-        </div>
-      </div>
-      <div class="profilActivity" v-if="edit === false">
-        <p><span>Dernier sujet:</span> {{ userProfil.dataDb.lastPost }}</p>
-        <p><span>Dernier message:</span> {{ userProfil.dataDb.lastMessage }}</p>
+      <div v-else>
+        <h1>{{ userProfile.username }}</h1>
+        <span>{{ userProfile.createdAt }}</span>
+        <p v-if="!edit">{{ userProfile.bio }}</p>
       </div>
     </div>
   </section>
@@ -57,33 +33,28 @@ export default {
   name: "UserProfile",
   data() {
     return {
-      userProfil: {
-        name: "Palpatine",
-        firstName: "Sheev",
-        userName: "EDF",
-        subscriptionDate: "11/11/1111",
-        job: "Empereur",
-        bio: "J'aime la peinture, les popcorns et découvrir de nouveaux talents",
-        dataDb: {
-          lastPost: "Des Frites à la cantine",
-          lastMessage: "Oui mais cuite façon Belge!",
-        },
-      },
+      userProfile: null,
       edit: false,
     };
+  },
+  async beforeMount() {
+    const response = await this.getProfile();
+    this.userProfile = response.data;
+    return;
   },
   computed: {},
   methods: {
     changeEdit() {
-      if (this.edit === true) {
-        return (this.edit = false);
-      }
-      if (this.edit === false) {
-        return (this.edit = true);
-      }
+      this.edit = !this.edit;
     },
-    prout() {
-      console.log("prout");
+    async getProfile() {
+      const token = localStorage.getItem("token");
+      return this.axios.get("http://localhost:3000/api/user/profile", {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
     },
   },
 };
@@ -94,21 +65,21 @@ $lightblueColor: #3e8af7;
 $primaryColor: #fd2d02;
 $validationColor: #4e920e;
 
-#editBtn {
+.editBtn {
   background-color: $lightblueColor;
   margin: 0 auto 10px auto;
   &:hover {
     background-color: darken($lightblueColor, 10);
   }
 }
-#deleteBtn {
+.deleteBtn {
   background-color: $primaryColor;
   margin: auto;
   &:hover {
     background-color: darken($primaryColor, 10);
   }
 }
-#validationBtn {
+.validationBtn {
   background-color: $validationColor;
   margin: auto;
   &:hover {
@@ -127,20 +98,13 @@ $validationColor: #4e920e;
         border-radius: 50%;
       }
     }
-    .profilEditBtn {
-      width: 50%;
-      button {
-        color: white;
-        display: block;
-        border: none;
-        border-radius: 10px;
-        margin-bottom: 20px;
-        padding: 10px 5px;
-        a {
-          color: white;
-          text-decoration: none;
-        }
-      }
+    .profilBtn {
+      color: white;
+      display: block;
+      border: none;
+      border-radius: 10px;
+      margin-bottom: 20px;
+      padding: 10px 5px;
     }
     @media (min-width: 768px) {
       width: 50%;
@@ -166,10 +130,10 @@ $validationColor: #4e920e;
         font-weight: bold;
       }
       .editInput {
-          input {
-              border: none;
-              border-bottom: solid 1px lightgrey;
-          }
+        input {
+          border: none;
+          border-bottom: solid 1px lightgrey;
+        }
       }
     }
     .profilActivity {
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import LoginPage from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginPage from "../views/Login.vue";
+import SignupComponent from "../views/Signup.vue";
+import UserProfile from "../views/User.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: LoginPage
+    path: "/user/login",
+    name: "Login",
+    component: LoginPage,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/user/signup",
+    name: "Signup",
+    component: SignupComponent,
+  },
+  {
+    path: "/user/profile",
+    name: "User",
+    component: UserProfile
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
