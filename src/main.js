aa9148fe127e377499c08ea6611a0506375f4b22
camille <camille@cstar.io>
2021-09-14 11:42:56 +0200

    update package main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
