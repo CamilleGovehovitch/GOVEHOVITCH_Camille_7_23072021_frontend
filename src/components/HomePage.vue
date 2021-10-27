8880e9fd3f33f3012d5b205272dbe3c484faeeeb
camille <camille@cstar.io>
2021-10-27 21:03:33 +0200

    update hom

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,13 +4,13 @@
       <div class="post" v-for="post in posts" :key="post.id">
         <PostTemplate :post="post" />
       </div>
-      <div class="createPostContainer">
+      <!-- <div class="createPostContainer">
         <router-link to="/post/new">
           <div class="createPost">
             <i class="fas fa-plus"> </i>
           </div>
         </router-link>
-      </div>
+      </div> -->
     </section>
   </div>
 </template>
@@ -28,27 +28,19 @@ export default {
       posts: null,
     };
   },
-  beforeCreate() {
+  async beforeMount() {
     const token = localStorage.getItem("token");
     if (!token) {
       window.location.href = "#/user/login";
     }
-  },
-  async beforeMount() {
     const response = await this.getPostsFromApi();
     this.posts = response.data;
     console.log(response);
   },
   computed: {
-    // ...mapGetters(['getProfile'])
   },
   methods: {
     getPostsFromApi() {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        // window.location.href = "/user/login";
-        console.log("empty");
-      }
       return this.axios.get("http://localhost:3000/api/post?limit=10&offset=1", {
         headers: {
           Accept: "application/json",
@@ -61,20 +53,15 @@ export default {
       const thumbsUps = document.querySelector(".fa-thumbs-up");
 
       if (!thumbsUps.classList.contains("liked")) {
-        console.log("green");
         thumbsUps.classList.add("liked");
         this.sendLikeToApi();
       } else {
         thumbsUps.classList.remove("liked");
-        console.log("not green");
       }
 
-      console.log(thumbsUps);
     },
     dislike() {
-      console.log("dislike");
       const thumbsDown = document.querySelector(".fa-thumbs-down");
-      console.log(thumbsDown);
       thumbsDown.classList.contains("disliked") ? thumbsDown.classList.remove("disliked") : thumbsDown.classList.add("disliked");
     },
   },
@@ -84,32 +71,6 @@ export default {
 <style lang="scss" scoped>
 $primaryColor: #fd2d02;
 
-// .header {
-//   background-color: lighten($primaryColor, 45%);
-//   display: flex;
-//   justify-content: space-between;
-//   padding: 10px 5px;
-//   margin-bottom: 15px;
-//   .profileName {
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     span {
-//       font-size: 20px;
-//       font-weight: Bold;
-//       color: $primaryColor;
-//     }
-//     h1 {
-//       color: darken($primaryColor, 10%);
-//     }
-//   }
-//   .profilePictureContainer {
-//     img {
-//       width: 100px;
-//       border-radius: 50%;
-//     }
-//   }
-// }
 .main {
   background-color: lighten($primaryColor, 45%);
   .postContainer {
@@ -131,8 +92,6 @@ $primaryColor: #fd2d02;
     text-decoration: none;
     color: darken($primaryColor, 10%);
     text-align: end;
-    // bottom: 35px;
-    // right: 35px;
     .createPost {
       background-color: white;
       border-radius: 50%;
@@ -158,8 +117,8 @@ $primaryColor: #fd2d02;
         height: 70px;
       }
       @media (min-width: 2560px) {
-        width: 50px;
-        height: 50px;
+        width: 200px;
+        height: 200px;
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,100 @@
 <template>
   <div class="home">
-    <div>
+    <div class="content-container">
       <UserPanel />
-      <!-- <HomePage /> -->
-      <div class="post" v-for="post in posts" :key="post.id">
-        <PostTemplate :post="post" />
+
+      <div class="paginationContainerTop">
+        <button v-for="page in pages()" :key="page.id" @click="changePage(page)" class="pagination" :class="offset === page ? 'currentPage' : ''">
+          {{ page }}
+        </button>
       </div>
+
+      <div class="post" v-for="post in posts" :key="post.id">
+        <PostTemplate :post="post" @delete-sent="deletePostFromApi" />
+      </div>
+      <button v-for="page in pages()" :key="page.id" @click="changePage(page)" class="pagination" :class="offset === page ? 'currentPage' : ''">
+        {{ page }}
+      </button>
     </div>
     <div class="createPostContainer">
-      <router-link to="/post/new">
-        <div class="createPost">
-          <i class="fas fa-plus"> </i>
-        </div>
-      </router-link>
+      <ButtonCreatePost />
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import UserPanel from "@/components/UserPanel.vue";
 import PostTemplate from "@/components/PostTemplate.vue";
-
+import ButtonCreatePost from "@/components/ButtonCreatePost.vue";
 
 export default {
   name: "Home",
   components: {
     UserPanel,
     PostTemplate,
+    ButtonCreatePost,
   },
   data() {
     return {
       posts: null,
-      pages: null,
+      postsCount: null,
+      offset: 1,
+      limit: 10,
     };
   },
   async beforeMount() {
-    this.$store.commit("LOADING_SPINNER_SHOW_MUTATION", true);
-    const postsResponse = await this.getPostsFromApi();
-    this.$store.commit("LOADING_SPINNER_SHOW_MUTATION", false);
-
-    this.posts = postsResponse.data.rows;
-    this.pages = postsResponse.data.count;
-  },
-  mounted() {
     this.checkToken();
+    await this.fetchPost();
   },
-  computed: {},
   methods: {
     checkToken() {
       const token = localStorage.getItem("token");
-
       if (!token) {
-        console.log("empty token");
-        return (window.location.href = "#/user/login");
+        localStorage.clear();
+        window.location.href = "#/user/login";
       }
     },
     getPostsFromApi() {
       const token = localStorage.getItem("token");
-      return this.axios.get("http://localhost:3000/api/post?limit=10&offset=1", {
+      return this.axios.get("http://localhost:3000/api/post?limit=" + this.limit + "&offset=" + this.offset, {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    },
+    pagesCount() {
+      return Math.ceil(this.postsCount / this.limit);
+    },
+    pages() {
+      let pages = [];
+      for (let i = 1; i <= this.pagesCount(); i++) {
+        pages.push(i);
+      }
+      return pages;
+    },
+    async changePage(page) {
+      this.offset = page;
+      await this.fetchPost();
+    },
+    //FETCH POST
+    async fetchPost() {
+      this.$store.commit("LOADING_SPINNER_SHOW_MUTATION", true);
+      const postsResponse = await this.getPostsFromApi();
+      this.$store.commit("LOADING_SPINNER_SHOW_MUTATION", false);
+
+      this.posts = postsResponse.data.rows;
+      this.postsCount = postsResponse.data.count;
+    },
+    async deletePostFromApi(payload) {
+      const response = await this.sendDeletePostToApi(payload.postId);
+      if (response) {
+        this.fetchPost();
+      }
+    },
+    sendDeletePostToApi(postId) {
+      const token = localStorage.getItem("token");
+      return this.axios.delete("http://localhost:3000/api/post/" + postId, {
         headers: {
           Accept: "application/json",
           Authorization: "Bearer " + token,
@@ -66,22 +102,67 @@ export default {
       });
     },
   },
-
-  //appel fetch pr rafraichir les posts et les stocker ds un data, créer ta vue pr instancier les nouveaux posts
 };
 </script>
 
 <style lang="scss" scoped>
 $primaryColor: #fd2d02;
 
+@import "../sass/utils/reset.scss";
+
+@keyframes translatePost {
+  from {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+.currentPage {
+  background-color: lighten($primaryColor, 20%);
+}
+.pagination {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  margin-right: 5px;
+  margin-bottom: 20px;
+  &:hover {
+    background-color: lighten($primaryColor, 30%);
+  }
+}
+
+.home {
+  background-image: url("../assets/images/icon.png");
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  .paginationContainerTop {
+    padding-top: 180px;
+    @media (min-width: 1440px) {
+      padding-top: 200px;
+    }
+  }
+}
+
 .post {
+  width: 90%;
+  margin: auto;
   border: solid 1px darken($primaryColor, 10%);
   margin-bottom: 20px;
-  padding: 10px 5px;
   box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.3);
   transition: box-shadow ease-in-out 330ms;
+  animation: translatePost 600ms 500ms both;
   &:hover {
-    box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.53);
+    box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.8);
+  }
+  @media (min-width: 1024px) {
+    width: 80%;
+    margin: auto;
+    margin-bottom: 20px;
   }
 }
 .createPostContainer {
@@ -92,36 +173,6 @@ $primaryColor: #fd2d02;
     text-decoration: none;
     color: darken($primaryColor, 10%);
     text-align: end;
-
-    .createPost {
-      background-color: white;
-      border-radius: 50%;
-      border: solid 2px darken($primaryColor, 10%);
-      width: 50px;
-      height: 50px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      box-shadow: 5px 5px 15px 1px rgba(0, 0, 0, 0.29);
-      transition: ease-in-out 300ms;
-      &:hover {
-        background-color: darken($primaryColor, 10%);
-        color: white;
-      }
-      @media (min-width: 1024px) {
-        font-size: 25px;
-        width: 70px;
-        height: 70px;
-      }
-      @media (min-width: 1440px) {
-        width: 70px;
-        height: 70px;
-      }
-      @media (min-width: 2560px) {
-        width: 50px;
-        height: 50px;
-      }
-    }
   }
 }
 </style>
