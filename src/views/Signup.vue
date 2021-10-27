f89df05c114206ab88a4fbd0c2cc21a837fb7e7e
camille <camille@cstar.io>
2021-10-27 21:05:03 +0200

    update signup with error message from api

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <SignupComponent email="Email" password="Password" title="GROUPOMANIA" @subscription-data="userSubscription" />
+    <SignupComponent :email="email" :password="password" :title="title" @subscription-data="userSubscription"/>
+    <p class="error" v-if="errorMessage">{{ errorMessage }}</p>
+    <p class="error" v-for="error in errors" :key="error">{{ error }}</p>
   </div>
 </template>
 
@@ -13,28 +15,66 @@ export default {
     SignupComponent,
   },
   data() {
-    return {};
+    return {
+      email: "Email",
+      password: "Password",
+      title: "GROUPOMANIA",
+      errorMessage: "",
+      newUser: null,
+      errors: [],
+    };
   },
+  props: {},
   methods: {
-    async userSubscription(payload) {
-      console.log("hello");
+    // validationInput(e, payload) {
+    //   e.preventDefault();
+    //   const userEmail = payload.userEmail;
+    //   const userPassword = payload.userPassword;
+    //   const confirmPassword = payload.confirmPassword;
+    //   const emailRegExp = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]{2,}\\.[a-z]{2,4}$");
 
+    //   if (userEmail === "" || userPassword === "" || confirmPassword === "") {
+    //     return (this.errorEmptyMessage = true);
+    //   }
+    //   if (!emailRegExp.test(userEmail)) {
+    //     return (this.errorMatchEmail = true);
+    //   }
+    //   if (confirmPassword !== userPassword) {
+    //     return (this.errorUnmatchMessage = true);
+    //   }
+    //   this.userSubscription(payload);
+    // },
+    userSubscription( payload) {
       const bodyReq = {
         email: payload.userEmail,
         password: payload.userPassword,
       };
 
-      const response = await this.axios.post("http://localhost:3000/api/user/signup", bodyReq, {
-        headers: {
-          Accept: "application/json",
-        },
-      });
-      console.log(response.data);
-      if (response.data.userId) {
-        localStorage.setItem("token", response.data.token);
-        window.location.href = "/user/login";
-      }
+      this.axios
+        .post("http://localhost:3000/api/user/signup", bodyReq, {
+          headers: {
+            Accept: "application/json",
+          },
+        })
+        .then((newUser) => {
+          this.errorMessage = "";
+          this.newUser = newUser.data;
+          window.location.href = "#/user/login";
+        })
+        .catch((error) => {
+          this.errors = [];
+          this.errors.push(error.response.data.error);
+          this.errorMessage = error.response.data.message;
+        });
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+$primaryColor: #fd2d02;
+.error {
+  color: darken($primaryColor, 10%);
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
